f22be895c29368311691782a3f29dc0399e3e8f6
Carl Witt <wittcarl@informatik.hu-berlin.de>
2018-11-22 18:22:07 +0100

    Scrape for regular use with cron

---
+++ scrape.sh
@@ -0,0 +1,8 @@
+# get raw data from amazon
+curl https://d2xn1uj035lhvj.cloudfront.net/pricing/1.0/ec2/region/eu-west-2/ondemand/linux/index.json?timestamp=1542899708728 | gzip > $(date +%s).json.gz
+
+# extract instance type attributes and prices as csv
+# probably would have been easier to do this in python, importing the json 
+# manual: run extract-instance-info.xquery on raw data > instance-info.csv
+
+# manual: run instance-types.R script to visualize
